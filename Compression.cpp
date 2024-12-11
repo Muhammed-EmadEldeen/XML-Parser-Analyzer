cc2fcc0aa3c543e291cd8f9ad702dee30051590d
mohamedsameh20 <2100401@eng.asu.edu.eg>
2024-12-11 19:25:13 +0200

    splitting..

--- Compression.cpp
+++ Compression.cpp
@@ -105,24 +105,8 @@ string byte_pair_compress(const string& input) {
     return compressed;
 }
 
-int main(int argc, char** argv) {
-    string mode = argv[1];
-    string input_file = argv[3];
-    string output_file = argv[5];
-    string content = read_file(input_file);
-    string result;
 
-    if (mode == "compress") {
-        result = byte_pair_compress(content);
-    }
-    write_file(output_file, result);
-    if (mode == "compress") {
-        for (const auto& pair : mapping) {
-            cout << pair.first << " -> " << pair.second << endl;
-        }
-    }
-    return 0;
-}
+
 
 
 
