41f690ebc6c6c208d95e6992ef719581ac67b394
Mohamed-Elahmar <mohamed1elahmar@gmail.com>
2024-12-30 19:26:42 +0200

    make the User Interface of the XML Analyzer and make the actions of the buttons

--- GUIOfBothPtojects/analyzerwindow.cpp
+++ GUIOfBothPtojects/analyzerwindow.cpp
@@ -1,6 +1,12 @@
 #include "analyzerwindow.h"
 #include "ui_analyzerwindow.h"
+#include "User.h"
+#include "UsersData.h"
 
+string filePath;
+UsersData UsersDataOne;
+User MostFollowedUser;
+User MostFollowingUser;
 
 AnalyzerWindow::AnalyzerWindow(QWidget *parent)
     : QDialog(parent)
@@ -13,3 +19,34 @@ AnalyzerWindow::~AnalyzerWindow()
 {
     delete ui;
 }
+
+void AnalyzerWindow::on_dirBtn_clicked()
+{
+    QString dir = QFileDialog::getExistingDirectory(this, tr("Select Directory"));
+    filePath =dir.toStdString();
+    ui->pathLabel->setText(dir);
+}
+
+
+
+void AnalyzerWindow::on_getMostFollowedUserBtn_clicked()
+{
+    MostFollowedUser= UsersDataOne.getMostFollowedUser();
+    ui->getMostFollowedUserLbl->setText(QString::fromStdString(MostFollowedUser.getName()));
+}
+
+
+void AnalyzerWindow::on_getMostFollowingUserBtn_clicked()
+{
+    MostFollowingUser= UsersDataOne.getMostFollowedUser();
+    ui->getMostFollowedUserLbl->setText(QString::fromStdString(MostFollowingUser.getName()));
+}
+
+
+void AnalyzerWindow::on_parseXmlFileBtn_clicked()
+{
+    UsersDataOne.parseXmlFile(filePath);
+    ui->parseXmlFileLbl->setText("Done");
+
+}
+
