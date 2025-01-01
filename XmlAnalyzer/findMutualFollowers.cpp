7d1aa9692f15c15aeefbfc1c895ba58c3f1d3407
MohamedAtta22 <109322691+MohamedAtta22@users.noreply.github.com>
2025-01-01 06:24:52 +0200

    MutualFollowers Function
    MutualFollowers Function was Added

---
+++ XmlAnalyzer/findMutualFollowers.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <fstream>
+#include <algorithm>  
+using namespace std;
+    vector<string> findMutualFollowers(vector<string> userIds) {
+        if (userIds.empty()) {
+            return vector<string>();
+        }       
+        // Count how many times each follower appears
+        map<string, int> followerCount;        
+        // For each user in our input list
+        for (string userId : userIds) {
+            // Get their followers
+            if (users.find(userId) != users.end()) {
+                for (string followerId : users[userId].followers) {
+                    followerCount[followerId]++;
+                }
+            }
+        }      
+        // Find followers who follow all users in our input list
+        vector<string> mutualFollowers;
+        for (auto const& pair : followerCount) {
+            // If follower count equals number of input users, they follow all users
+            if (pair.second == userIds.size()) {
+                mutualFollowers.push_back(pair.first);
+            }
+        }   
+        return mutualFollowers;
+    }
