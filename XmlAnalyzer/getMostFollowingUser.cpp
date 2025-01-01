0ad3e9c13ac914671881ea0ff6d7aa20d7a203b9
Mohamed Sameh <167581011+mohamedsameh20@users.noreply.github.com>
2025-01-01 17:13:22 +0200

    Update getMostFollowingUser.cpp

--- XmlAnalyzer/getMostFollowingUser.cpp
+++ XmlAnalyzer/getMostFollowingUser.cpp
@@ -1,22 +1,12 @@
 #include "UsersData.h"
 #include <iostream>
 using namespace std;
+
 User UsersData::getMostFollowingUser() {
     User* mostFollowingUser = nullptr;
     int maxFollowing = 0;
-
-    for (auto& user : users) {
-        int followingCount = 0;
-
-        for (auto& otherUser : users) {
-            vector<User> followers = otherUser.getFollowers();
-            for (auto& follower : followers) {
-                if (follower.getID() == user.getID()) {
-                    followingCount++;
-                    break;
-                }
-            }
-        }
+    for(auto& user : users) {
+    int followingCount = user.getFollowers().size();
         if (followingCount > maxFollowing) {
             maxFollowing = followingCount;
             mostFollowingUser = &user;
