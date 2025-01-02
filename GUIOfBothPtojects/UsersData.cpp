63f77e94eaed81b2ab8afbebaadc335f8a522b37
Mohamed Emad <108807444+Muhammed-EmadEldeen@users.noreply.github.com>
2025-01-02 13:43:12 +0200

    Update UsersData.cpp

--- GUIOfBothPtojects/UsersData.cpp
+++ GUIOfBothPtojects/UsersData.cpp
@@ -12,10 +12,67 @@ using namespace std;
     User UsersData::getMostFollowedUser(){}
     User UsersData::getMostFollowingUser(){}
     //vector<User> UsersData::mutualFollowers(User user1, User user2){}
-    vector<string> UsersData::findMutualFollowers(vector<string> userIds){}
+User *UsersData::findUserById(int id) {
+  for (User &u : users) {
+    if (u.getID() == id) {
+      return &u;
+    }
+  }
+  return nullptr;
+}
 
-    vector<Post> UsersData::getPostsByWord(string word){}
-    vector<Post> UsersData::getPostsByTopic(string topic){}
+vector<string> UsersData::findMutualFollowers(vector<string> userIds) {
+  map<int, int> followers;
+  vector<string> answer;
+
+  for (string id : userIds) {
+    User *currentUser = findUserById(stoi(id));
+    if (!currentUser) {
+      cout << "Error wrong ID" << endl;
+      return answer;
+    }
+    for (User f : currentUser->getFollowers()) {
+      followers[f.getID()]++;
+    }
+  }
+  for (pair<int, int> e : followers) {
+    if (e.second == userIds.size()) {
+      answer.push_back(to_string(e.first));
+    }
+  }
+  return answer;
+}
+
+
+vector<Post> UsersData::getPostsByWord(string searchWord) {
+    vector<Post> matchingPosts;
+    User* usersearch = nullptr;
+    for (User& user : users) {
+        for (Post& post : user.getPosts()) {
+            if (post.body.find(searchWord) != string::npos || post.topic.find(searchWord) != string::npos) {
+                matchingPosts.push_back(post);
+            }
+        }
+    }
+    
+    return matchingPosts;
+}
+
+vector<Post> UsersData::getPostsByTopic(string searchTopic) {
+    vector<Post> matchingPosts;
+    // vector<User> usersposts = users;
+    User* usersss = nullptr;
+    for (User& user : users) {
+        for (Post& post : user.getPosts()) {
+            if (post.topic == searchTopic) {
+                matchingPosts.push_back(post);
+            }
+        }
+    }
+    
+    return matchingPosts;
+}
+
     vector<vector<int>> UsersData::getFollowersMatrix(){
 
         vector<vector<int>> theFollowersVector;
@@ -40,168 +97,149 @@ using namespace std;
         return theFollowersVector;
     }
     //
-    void UsersData::getUsersFromXml (const string& filePath){}
+    User userAnalyze(ifstream &file);
+
+using namespace std;
+
+string UsersData::extractFromTag(const string &line, const string &tag) {
+  size_t start = line.find("<" + tag + ">");
+  size_t end = line.find("</" + tag + ">");
+  if (start != string::npos && end != string::npos) {
+
+    start += tag.size() + 2;
+    return line.substr(start, end - start);
+  }
+  return "";
+}
+
+User UsersData::userAnalyze(ifstream &file) {
+  string line;
+
+  User user;
+  user.setId(0);
+
+  while (getline(file, line)) {
+    if (line.find("</user>") != string::npos) {
+      break;
+    }
+
+    if (line.find("<name>") != string::npos) {
+      user.setName(extractFromTag(line, "name"));
+    }
+    if (line.find("<id>") != string::npos) {
+      user.setId(stoi(extractFromTag(line, "id")));
+    }
+
+    if (line.find("<follower>") != string::npos) { /// list of followers
+                                                   /// detected
+
+      string followerId;
+
+      while (getline(file, line)) {
+        if (line.find("<id>") != string::npos) {
+
+          followerId = extractFromTag(line, "id");
+        }
+        if (!followerId.empty()) {
+          int idStringToInt = stoi(followerId);
+          ;
+          /*
+             follower.setId(stoi(followerId)) ;
+             user.pushFollower(follower) ;*/
+          bool repeatedId = false;
+          for (int i = 0; i < (int)user.getFollowers().size(); i++) {
+            if (user.getFollowers()[i].getID() == idStringToInt) {
+              repeatedId = true;
+              break;
+            }
+          }
+          if (!repeatedId) {
+            User follower;
+            follower.setId(idStringToInt);
+            user.pushFollower(follower);
+          }
+        }
+        if (line.find("</follower>") != string::npos)
+          break;
+      }
+    }
+    if (line.find("<post>") != string::npos) {
+      user.pushPost(analyzePost(file));
+    }
+  }
+
+  return user;
+}
+
+Post UsersData::analyzePost(ifstream &file) {
+
+  string line;
+  string postBody = "";
+  string topic = "";
+  bool inBody = false;
+
+  while (getline(file, line)) {
+
+    if (line.find("</post>") != string::npos) { /// end of followers list
+      return Post{topic, postBody};
+    }
+
+    if (line.find("<topic>") != string::npos) {
+      topic = extractFromTag(line, "topic");
+    }
+    if (!inBody && line.find("<body>") != string::npos) {
+      /// start accumulating the body in a string
+      inBody = true;
+      size_t start = line.find("<body>") + 6;
+      size_t end = line.find("</body>");
+      if (end != string::npos) {
+        postBody += line.substr(start, end - start);
+        inBody = false;
+      } else {
+        postBody += line.substr(start);
+      }
+
+      // body = extractFromTag(line, "body");
+
+    } else if (inBody) {
+      size_t end = line.find("</body>");
+      if (end != string::npos) {
+
+        postBody += line.substr(0, end);
+        inBody = false;
+      } else {
+
+        postBody += line;
+      }
+    }
+  }
+  return Post{topic, postBody};
+}
+
+//////////////////////////////////////////////////////////////Main Function
+void UsersData::getUsersFromXml(const string &filePath) {
+
+  ifstream file(filePath);
+  if (!file.is_open()) {
+    cout << "error opening the file";
+    return;
+  }
+
+  string line;
+  while (getline(file, line)) {
+
+    if (line.find("<user>") != string::npos) /// if a user is detected
+    {
+      this->users.push_back(userAnalyze(file));
+    }
+  }
+  file.close();
+  return;
+}
 
 
 
 
 
-    //temp
-    //  string extractFromTag (const string & line , const string &tag);
 
 
-    // User userAnalyze (ifstream& file );
-    // Post analyzePost(ifstream& file);
-
-
-    // using namespace std;
-
-
-    // string extractFromTag (const string & line , const string &tag){
-    //     size_t start = line.find("<" + tag + ">" )  ;
-    //     size_t end = line.find ("</" + tag + ">" );
-    //     if (start != string::npos && end!= string::npos ){
-
-    //         start += tag.size()+2 ;
-    //         return line.substr(start , end-start);
-    //     }
-    //     return "";
-    // }
-
-
-
-
-    // User userAnalyze (ifstream& file ){
-    //     string line ;
-
-    //     User user ;
-    //     user.setId(0);
-
-    //     while(getline(file ,line) ){
-    //         if (line.find("</user>") != string::npos){
-    //             break;
-    //         }
-
-    //         if(line.find("<name>") != string::npos){
-    //             user.setName ( extractFromTag(line , "name") );
-    //         }
-    //         if (line.find("<id>")  != string::npos ){
-    //             user.setId ( stoi(extractFromTag(line , "id") ) );
-    //         }
-
-    //         if(line.find("<follower>") != string::npos ){ ///list of followers detected
-
-    //             string followerId ;
-
-    //             while (getline(file, line) ){
-    //                 if(line.find("<id>") != string::npos){
-
-    //                     followerId = extractFromTag(line , "id");
-    //                 }
-    //                 if (!followerId.empty()){
-    //                     int idStringToInt = stoi(followerId);;
-    //                     /*
-    //                 follower.setId(stoi(followerId)) ;
-    //                 user.pushFollower(follower) ;*/
-    //                     bool repeatedId = false ;
-    //                     for(int i=0 ; i < (int)user.getFollowers().size() ; i++){
-    //                         if (user.getFollowers()[i].getID() == idStringToInt)
-    //                         {
-    //                             repeatedId = true ;
-    //                             break;
-    //                         }
-    //                     }
-    //                     if(!repeatedId){
-    //                         User follower ;
-    //                         follower.setId(idStringToInt);
-    //                         user.pushFollower(follower) ;
-    //                     }
-
-    //                 }
-    //                 if(line.find("</follower>") != string::npos )
-    //                     break;
-    //             }
-
-    //         }
-    //         if(line.find("<post>") != string::npos){
-    //             user.pushPost( analyzePost(file)  );
-    //         }
-
-    //     }
-
-
-    //     return user ;
-    // }
-
-
-    // Post analyzePost(ifstream& file){
-
-    //     string line ;
-    //     string postBody = "";
-    //     string topic = "";
-    //     bool inBody =false;
-
-    //     while (getline(file , line) ){
-
-    //         if (line.find("</post>") != string::npos ){ ///end of followers list
-    //             return Post{topic ,postBody} ;
-    //         }
-
-    //         if (line.find("<topic>") != string::npos){
-    //             topic = extractFromTag(line, "topic");
-
-    //         }
-    //         if ( !inBody && line.find("<body>") != string::npos){
-    //             ///start accumulating the body in a string
-    //             inBody = true ;
-    //             size_t start = line.find("<body>")+6;
-    //             size_t end = line.find("</body>");
-    //             if (end != string::npos){
-    //                 postBody += line.substr(start,end-start ) ;
-    //                 inBody = false;
-    //             }else {
-    //                 postBody += line.substr(start ) ;
-    //             }
-
-
-    //             // body = extractFromTag(line, "body");
-
-    //         }else if(inBody){
-    //             size_t end  = line.find("</body>") ;
-    //             if(end != string::npos ){
-
-    //                 postBody+= line.substr(0,end);
-    //                 inBody =false ;
-    //             }else {
-
-    //                 postBody+= line  ;
-    //             }
-
-    //         }
-    //     }
-    //     return Post{topic ,postBody} ;
-    // }
-
-    // //////////////////////////////////////////////////////////////Main Function
-    // void UsersData::getUsersFromXml  (const string& filePath){
-
-    //     ifstream file (filePath ) ;
-    //     if(! file.is_open()){
-    //         cout<<"error opening the file" ;
-    //         return ;
-    //     }
-
-    //     string line ;
-    //     while (getline(file , line)) {
-
-    //         if(line.find("<user>") != string::npos ) ///if a user is detected
-    //         {
-    //             this->users.push_back(userAnalyze(file));
-    //         }
-
-
-    //     }
-    //     file.close();
-    //     return  ;
-
-    // }
