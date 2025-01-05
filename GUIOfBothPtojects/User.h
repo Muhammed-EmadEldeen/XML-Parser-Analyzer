8babc530a08d046f7865021a5e15c8d5b9c25c96
Mohamed-Elahmar <mohamed1elahmar@gmail.com>
2025-01-05 20:26:10 +0200

    .exe

--- GUIOfBothPtojects/User.h
+++ GUIOfBothPtojects/User.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // #ifndef USER_H
 // #define USER_H
 
@@ -72,8 +71,7 @@
 // #endif
 
 
-=======
->>>>>>> 9f6a4a4974f8548a8d2ed61e20078b248d6b3b88
+
 
 #ifndef USER_H
 #define USER_H
@@ -104,27 +102,12 @@ public:
 
     string getName() { return name; }
 
-<<<<<<< HEAD
     void setName(string name) { this->name = name; }
 
     void setId(int id) { this->id = id; }
 
     void setFollowers(vector<User> followers) { this->followers = followers; }
-=======
-  void setName(string name) { this->name = name; }
 
-  void setId(int id) { this->id = id; }
-
-  void setFollowers(vector<User> followers) { this->followers = followers; }
-
-  void pushFollower(User follower) { this->followers.push_back(follower); }
-  void pushPost(Post post) { this->posts.push_back(post); }
-
-  vector<User> getFollowers() { return followers; }
-
-  vector<Post> getPosts() { return posts; }
-  User() {};
->>>>>>> 9f6a4a4974f8548a8d2ed61e20078b248d6b3b88
 
     void pushFollower(User follower) { this->followers.push_back(follower); }
     void pushPost(Post post) { this->posts.push_back(post); }
--- GUIOfBothPtojects/UsersData.h
+++ GUIOfBothPtojects/UsersData.h
@@ -43,7 +43,7 @@ class UsersData {
 public:
     vector<User> users;
 
-<<<<<<< HEAD
+
     User getMostFollowedUser();
     User getMostFollowingUser();
     vector<Post> getPostsByWord(string word);
@@ -57,21 +57,7 @@ public:
     void getUsersFromXml(const string &filePath);
 
     User *findUserById(int id);
-=======
-  User getMostFollowedUser();
-  User getMostFollowingUser();
-  vector<Post> getPostsByWord(string word);
-  vector<Post> getPostsByTopic(string topic);
-  vector<vector<int>> getFollowersMatrix();
-  string extractFromTag(const string &line, const string &tag);
-  User userAnalyze(ifstream &file);
-  Post analyzePost(ifstream &file);
-  vector<string> findMutualFollowers(vector<string> userIds);
-  vector<int> suggestFollow(int id);
-  void getUsersFromXml(const string &filePath);
 
-  User *findUserById(int id);
->>>>>>> 9f6a4a4974f8548a8d2ed61e20078b248d6b3b88
 };
 
 #endif
