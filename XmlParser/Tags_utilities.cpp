1a0777459d7a668f6dd19a789b72f719026d5122
Muhamed <muhamed.emadeldeen@gmail.com>
2025-01-06 17:13:10 +0200

    fix bug

--- XmlParser/Tags_utilities.cpp
+++ XmlParser/Tags_utilities.cpp
@@ -8,7 +8,7 @@ bool isClosingTag(const string &tag) { return tag[0] == '/'; }
 // Helper function to extract the name of a tag (without angle brackets)
 string getTagName(const string &tag) {
   if (isClosingTag(tag)) {
-    return tag.substr(2, tag.size() - 3); // Remove </ and >
+    return tag.substr(1, tag.size() - 2); // Remove </ and >
   }
-  return tag.substr(1, tag.size() - 2); // Remove < and >
+  return tag.substr(0, tag.size() - 1); // Remove < and >
 }
